bfe33f7698ad938efcaccc8557fce2b65bd02c79
Tshepang Masela <maselatshepang68@gmail.com>
2026-05-29 02:20:07 +0200

    Build personal profile page

---
+++ src/components/ProfileCard.tsx
@@ -0,0 +1,10 @@
+export default function ProfileCard() {
+    return (
+        <div>
+            <h1>Profile Card</h1>
+            <p>Name: Tshepang</p>
+            <p>Programe Name: Software Developer</p>
+            <p>Current skills: Python,HTML,CSS</p>
+        </div>
+    );
+}
